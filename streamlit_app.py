3092bedbdcb72f8f376ab6b136ef58617cc5c934
isandrade-udea <andradeisabelcristina@gmail.com>
2024-10-10 11:10:43 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,6 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-
 st.title(":bus: Cootracovi ")
 st.write(
-    "Empresa trasnportadora de las rutas "
+    "Empresa trasnportadora de las rutas: HONDA - ORIENTAL - HONDA"
 )
 
 # Cargar datos desde un enlace de GitHub
@@ -27,7 +22,11 @@ if url:
         st.error(f"Error al cargar el archivo: {e}")
 else:
     st.write("Por favor, introduce la URL de un archivo CSV en GitHub para continuar.")
+
+
 #preprocesamiento
+
+# prompt: eliminar la columna Unnamed: 10 y las filas vacias
 
 df = df.drop('Unnamed: 10', axis=1)
 df = df.dropna()
@@ -35,12 +34,92 @@ df = df.dropna()
 # Eliminar la fila donde se repiten los nombres de las columnas
 df = df[df['Fecha'] != 'Fecha']
 
+# prompt: cambiar formato de veichulos y pasajeros a int, viaje y kilometros a float, fecha
+
+df['Vehiculo'] = df['Vehiculo'].astype(int)
+df['Pasaj'] = df['Pasaj'].astype(int)
+df['Viaje'] = df['Viaje'].astype(float)
+df['Kms'] = df['Kms'].astype(float)
+
+df['Fecha_Hora_Salida'] = pd.to_datetime(df['Fecha'] + ' ' + df['Hora salida'])
+df['Fecha_Hora_Salida'] = pd.to_datetime(df['Fecha_Hora_Salida'], format='%Y-%m-%d %H:%M:%S')
+
+df['Fecha_Hora_Final'] = pd.to_datetime(df['Fecha'] + ' ' + df['Hora Final'])
+df['Fecha_Hora_Final'] = pd.to_datetime(df['Fecha_Hora_Final'], format='%Y-%m-%d %H:%M:%S')
+
+
+# Convertir la columna 'Hora' al tipo datetime
+
+def get_seconds(time_str):
+    #print('Time in hh:mm:ss:', time_str)
+    # split in hh, mm, ss
+    hh, mm, ss = time_str.split(':')
+    return int(hh) * 3600 + int(mm) * 60 + int(ss)
+
+#df['Hora_salida_s'] = df['Hora salida'].apply(get_seconds)
+df['Tiempo_viaje_s'] = df['Tiempo Viaje'].apply(get_seconds)
+df['Tiempo_viaje_s'] = df['Tiempo_viaje_s'].astype(float)
+df['Tiempo_muerto_s'] = df['Tiempo Muerto'].apply(get_seconds)
+df['Tiempo_muerto_s'] = df['Tiempo_muerto_s'].astype(float)
+
+# Generar una columna para el día
+df['Dia'] = df['Fecha_Hora_Salida'].dt.day
+# crear columna con numero de ruta
+df['n_ruta'] = df['Ruta'].str.split(' ', expand=True)[1].astype(float)
+
+# Eliminar la columna fecha, ruta, Hora salida,	Hora Final,	Tiempo Viaje,Tiempo Muerto
+#df = df.drop('Fecha', axis=1)
+
+df = df.drop('Ruta', axis=1)
+df = df.drop('Hora salida', axis=1)
+df = df.drop('Hora Final', axis=1)
+df = df.drop('Tiempo Viaje', axis=1)
+df = df.drop('Tiempo Muerto', axis=1)
+df = df.drop('Fecha', axis=1)
+
+# Crear la columna 'Hora' con la hora extraída de 'Fecha_Hora_Salida'
+df.loc[:, 'Hora'] = df['Fecha_Hora_Salida'].dt.hour
+
+# Función para clasificar las horas en jornada, incluyendo la madrugada
+def clasificar_jornada(hora):
+    if 0 <= hora < 6:
+        return 'Madrugada'
+    elif 6 <= hora < 12:
+        return 'Mañana'
+    elif 12 <= hora < 18:
+        return 'Tarde'
+    else:
+        return 'Noche'
+
+# Aplicar la función para crear la columna 'Jornada'
+df.loc[:, 'Jornada'] = df['Hora'].apply(clasificar_jornada)
+
+#Resumen
+
+# Borrar las filas donde la columna 'n_ruta' es igual0 a 2, 6 u 8
+df = df[~df['n_ruta'].isin([2, 6, 8])]
+
+# Eliminar las columnas 'Fecha_Hora_Final' y 'n_ruta'
+df = df.drop(['Fecha_Hora_Final', 'n_ruta','Viaje'], axis=1)
+
+# fijamos la columna como indice
+df = df.set_index('Fecha_Hora_Salida')
+
+# Paso 1: Eliminar índices duplicados, manteniendo la primera ocurrencia
+df = df[~df.index.duplicated(keep='first')]
+
 #Tamaño del dataset
 st.write(f"El tamaño del dataset es: {df.shape[0]} filas y {df.shape[1]} columnas.")
 
+# Análisis de la periodicidad del dataset
+df['df_time_diffs'] = df.index.to_series().diff().dt.total_seconds()
 
-        
-# Mostrar estadísticas descriptivas
-st.subheader("Estadísticas Descriptivas")
-st.write(df.describe())
 
+
+
+
+
+
+
+
+st.dataframe(df.head())
