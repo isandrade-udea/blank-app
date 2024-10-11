0fecbe08eb8d42a5431ed0443e4dd62e06c0aca1
isandrade-udea <andradeisabelcristina@gmail.com>
2024-10-10 19:54:12 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import seaborn as sns
-import plotly.graph_objects as go
-import plotly.io as pio
+#import plotly.graph_objects as go
+#import plotly.io as pio
 # Modelado y Forecasting
 import skforecast
 import lightgbm
